57356f1707a4291a05a8e3a12d7a95cd1f7b3897
bfjose08 <bfjose26@gmail.com>
2022-10-08 00:20:40 -0500

    Uso de librerias para reutilizar codigo

---
+++ 01-19-Librerias/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aritmetica;
+
+
+namespace _01_19_Librerias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 0;
+
+            r = Class1.Suma(2.3, 4.6);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aritmetica/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aritmetica
+{
+    public class Class1
+    {
+        public static double Suma(double pA, double pB)
+        {
+            return pA + pB;
+        }
+        public static double Resta(double pA, double pB)
+        {
+            return pA - pB;
+        }
+        public static double Multi(double pA, double pB)
+        {
+            return pA * pB;
+        }
+        public static double Div(double pA, double pB)
+        {
+            return pA / pB;
+        }
+    }
+}
